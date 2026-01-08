69e4f1c06f2e98c445723d8dfd40bd70c7ddef18
lukez <846254399@qq.com>
2026-01-08 15:15:57 +0800

    消息增加批量已读

--- src/Helpers/Message.php
+++ src/Helpers/Message.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
  */
 class Message
 {
-    
+
     public static function setReadByTemplate($templateEnName, $relateId, $uids)
     {
 
@@ -24,6 +24,32 @@ class Message
         DB::table('message_record_user')->where([
             'template_id' => $templateId,
             'relate_id' => $relateId,
+        ])->whereIn('uid', $uids)->update([
+            'status' => 'read',
+            'updated_at' => now(),
+        ]);
+        return true;
+    }
+
+    /**
+     * 根据模板英文名称和用户ID将所有相关消息标记为已读（不限制relateId）
+     *
+     * @param string $templateEnName 模板英文名称
+     * @param array|int $uids 用户ID列表或单个用户ID
+     * @return bool
+     */
+    public static function setAllReadByTemplate($templateEnName, $uids)
+    {
+        if (!is_array($uids)) {
+            $uids = [$uids];
+        }
+
+        $templateId = DB::table('message_template')->where('name_en', $templateEnName)->value('id');
+        if (!$templateId) {
+            return false;
+        }
+        DB::table('message_record_user')->where([
+            'template_id' => $templateId,
         ])->whereIn('uid', $uids)->update([
             'status' => 'read',
             'updated_at' => now(),
