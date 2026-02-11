ad6276f86cdf959bc9ddb7978e368fdfe737e8cb
dong <zhangld@enlightv.com>
2026-02-11 17:24:35 +0800

    系统日志输出及抛出

--- src/Exceptions/BusinessException.php
+++ src/Exceptions/BusinessException.php
@@ -9,7 +9,7 @@ class BusinessException extends \RuntimeException
     // 增加一个是否需要记录日志的属性
     public bool $shouldLog = false;
 
-    public function __construct(string $message, int $code = -1, bool $shouldLog = false)
+    public function __construct($message, $code = -1, bool $shouldLog = false)
     {
         parent::__construct($message, $code);
         $this->shouldLog = $shouldLog;
